eec5b70d2a235fc07e0104663038b485b7eb955a
Kasem <kasrashrash@informatik.hs-bremerhaven.de>
2025-02-25 16:30:46 +0100

    New hle

---
+++ HelloWorldBad.java
@@ -0,0 +1,11 @@
+public class HelloWorldBad {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    // Falsche Methodennamen-Konvention (Großbuchstabe am Anfang)
+    public void DoSomething() {
+        // ...
+    }
+}
+
