dbfe5560cd50887c738a969f2f25b7d7cec6a51a
Kasem Rashrash <kasrashrash@smail.hs-bremerhaven.de>
2025-02-25 16:46:15 +0100

    NewHi3

---
+++ Hi3.java
@@ -0,0 +1,11 @@
+public class Hi3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    // Falsche Methodennamen-Konvention (Großbuchstabe am Anfang)
+    public void MOSomething() {
+        // ...
+    }
+}
+
