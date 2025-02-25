0b522e1f110fcbee5b8d874be528904060e1626c
Kasem <kasrashrash@informatik.hs-bremerhaven.de>
2025-02-25 16:57:23 +0100

    Hi

---
+++ HiBad5.java
@@ -0,0 +1,11 @@
+public class HiBad5 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    // Falsche Methodennamen-Konvention (Großbuchstabe am Anfang)
+    public void MOSomething() {
+        // ...
+    }
+}
+
